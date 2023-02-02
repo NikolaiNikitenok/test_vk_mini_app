8681925fb724804375feb5917398f87c7b07f339
NikolaiNikitenok <k89095867511@gmail.com>
2023-02-02 21:24:28 +0300

    add learn React

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+	const name = 'user';
+	const age  = '30';
+	
+	return <div>
+		name: {name}
+    <br/>
+		age:  {age}
+	</div>;
+}
+
+export default App;
