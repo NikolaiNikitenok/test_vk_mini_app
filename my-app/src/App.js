cca7697730640b8fefb6db256742f3a056d8885c
Nikolai Nikitenok <104710310+NikolaiNikitenok@users.noreply.github.com>
2023-02-04 19:20:34 +0300

    fr

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,15 +1,31 @@
 import React from 'react';
 
+const prods = [
+	{id: 1, name: 'product1', cost: 100},
+	{id: 2, name: 'product2', cost: 200},
+	{id: 3, name: 'product3', cost: 300},
+];
+
 function App() {
 
-  function showMess(number) {
-		alert(number);
-	}
+	const rows = prods.map(function(item) {
+		return <tr key={item.id}>
+			<td>{item.name}</td>
+			<td>{item.cost}</td>
+		</tr>;
+	});
 	
-	return <div>
-		<button onClick={() => showMess(1)}>act1</button> 
-		<button onClick={() => showMess(2)}>act2</button> 
-		<button onClick={() => showMess(3)}>act3</button> 
-	</div>;
+	return <table>
+		<thead>
+			<tr>
+				<td>название</td>
+				<td>стоимость</td>
+			</tr>
+		</thead>
+		<tbody>
+			{rows}
+		</tbody>
+	</table>;
 }
+
 export default App;
